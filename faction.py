8c4576b2f1611b27673359c9dd63383a6b4f61bf
Mohammadamirr <mabashardoust@gmail.com>
2025-02-08 16:03:44 +0330

    basic mchine learning

--- faction.py
+++ faction.py
@@ -8,3 +8,25 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('articles.csv')
+
+X_train, X_test, y_train, y_test = train_test_split(data['content'], data['dynasty'], test_size=0.2, random_state=42)
+
+vectorizer = TfidfVectorizer()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+model = MultinomialNB()
+model.fit(X_train_tfidf, y_train)
+
+y_pred = model.predict(X_test_tfidf)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
