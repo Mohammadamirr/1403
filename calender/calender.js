dd6e5acfdcb9cdc6ff757d02ee7d32d73e03a8a5
Mohammadamirr <mabashardoust@gmail.com>
2025-02-11 12:46:39 +0330

    add & commit this folder

---
+++ calender/calender.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        locale: 'fa',
+        timeZone: 'Asia/Tehran',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+    });
+
+    calendar.render();
+});
