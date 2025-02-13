5e83da6ae6ba5c4aa4ca29084ec52413fd5b8b7e
Mohammadamirr <mabashardoust@gmail.com>
2025-02-13 16:55:26 +0330

    commit search bar js

--- index/index.js
+++ index/index.js
@@ -1,1 +1,15 @@
+const searchInput = document.getElementById('search-input');
+const pagesList = document.getElementById('pages-list');
+const pages = pagesList.getElementsByTagName('li');
 
+searchInput.addEventListener('keyup', () => {
+    const filter = searchInput.value.toLowerCase();
+    Array.from(pages).forEach((page) => {
+        const text = page.textContent.toLowerCase();
+        if (text.includes(filter)) {
+            page.style.display = '';
+        } else {
+            page.style.display = 'none';
+        }
+    });
+});
